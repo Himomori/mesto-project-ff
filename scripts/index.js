c4353789e6baaf70e49914371c35f6ac01cf271b
Violetta Lomakina <Himomori@bk.ru>
2023-12-27 21:27:25 +1100

    реализованы карточки и их удаление

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector("#card-template").content;
+
+const renderCard = initialCards.map(function (element) {
+  return {
+    name: element.name,
+    link: element.link,
+  };
+});
+
+function render() {
+  renderCard.forEach(addCard);
+}
+
+function addCard(element) {
+  const cardElement = document.querySelector("#card-template").cloneNode(true);
+  cardElement.content.querySelector(".card__title").textContent = element.name;
+  cardElement.content.querySelector(".card__image").src = element.link;
+  const deleteButton = cardElement.content.querySelector(
+    ".card__delete-button"
+  );
+  deleteButton.addEventListener("click", removeCard);
+  document.querySelector(".places__list").append(cardElement.content);
+}
+function removeCard(element) {
+  element.target.closest(".card").remove();
+}
+
+render();
